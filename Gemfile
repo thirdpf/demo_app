43aa0eebae919521c63bbedd50c9d3813099a3e2
kyhome <melody_breeze86@docomo.ne.jp>
2013-10-27 14:16:44 +0900

    Initial commit kyhome

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+# Use sqlite3 as the database for Active Record
+group :development do
+  gem 'sqlite3'
+end
+
+# Use SCSS for stylesheets
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'coffee-rails', '~> 4.0.0'
+
+  gem 'uglifier', '>= 1.3.0'
+end
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+
+group :production do
+  gem 'pg','0.17.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '4ddd8ecd7f5773582b839516386dee44d3259085c90aaeb11514acdcfe948ebc80c1aa50a35cac0f10a4d18c6f5abd3fec7612fa09df63e3cf2124d43968b4d9'
